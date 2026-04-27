48ed9378bde650736a028212e98ac71f76af5f03
liuchenhao <liuchenhao@creality.com>
2026-04-27 09:14:44 +0800

    CGAL 6.1

--- deps/CGAL/CGAL.cmake
+++ deps/CGAL/CGAL.cmake
@@ -7,9 +7,9 @@ orcaslicer_add_cmake_project(
     # GIT_REPOSITORY https://github.com/CGAL/cgal.git
     # GIT_TAG        bec70a6d52d8aacb0b3d82a7b4edc3caa899184b # releases/CGAL-5.0
     # For whatever reason, this keeps downloading forever (repeats downloads if finished)
-    URL      https://github.com/CGAL/cgal/archive/refs/tags/v5.4.zip
-    URL_HASH SHA256=d7605e0a5a5ca17da7547592f6f6e4a59430a0bc861948974254d0de43eab4c0
-    PATCH_COMMAND git apply ${CGAL_DIRECTORY_FLAG} --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-clang19.patch
+    URL      https://github.com/CGAL/cgal/archive/refs/tags/v6.1.zip
+    URL_HASH SHA256=AC8F61BEF8A7D8732D041D0953DB3203C93427B1346389C57FA4C567E36672D4
+    # PATCH_COMMAND git apply ${CGAL_DIRECTORY_FLAG} --verbose --ignore-space-change --whitespace=fix ${CMAKE_CURRENT_LIST_DIR}/0001-clang19.patch
     DEPENDS dep_Boost dep_GMP dep_MPFR
 )
 
